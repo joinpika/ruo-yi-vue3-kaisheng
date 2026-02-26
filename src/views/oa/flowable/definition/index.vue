2175d8fd90afa6cc963f53d0c32f33e13461b1a7
卡卡西 <1258012958@qq.com>
2026-02-26 10:14:36 +0800

    add

--- src/views/oa/flowable/definition/index.vue
+++ src/views/oa/flowable/definition/index.vue
@@ -4,7 +4,7 @@
       <el-form-item label="名称" prop="name">
         <el-input
           v-model="queryParams.name"
-          placeholder="请输入名称"
+          placeholder="请输入名称1"
           clearable
           size="small"
           @keyup.enter.native="handleQuery"
